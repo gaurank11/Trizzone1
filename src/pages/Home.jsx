c12d04a3adaf6a4f1779369cac5032af50cbc1fa
Gaurank Sharma <95328684+gaurank11@users.noreply.github.com>
2025-03-26 01:40:26 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,12 @@ import { useEffect, useState } from "react";
 import Hero from "../components/hero"; // Importing the Hero component
 
 const Home = () => {
-  const [showHero, setShowHero] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setShowHero(true);
-    }, 1000); // Delay before showing the Hero component
-  }, []);
+ 
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-black text-white text-[70px] md:text-[280px] font-bold relative">
-      {/* Show Hero Component after delay */}
-      {showHero && <Hero />}
+  
+     <Hero />
     </div>
   );
 };
